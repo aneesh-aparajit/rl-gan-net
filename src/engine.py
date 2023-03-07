fd3c01bb47412a4601ad7a71045bc1b1c682acc3
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-03-08 01:50:13 +0530

    added tensorflow version

--- src/engine.py
+++ src/engine.py
@@ -6,16 +6,28 @@ from torch.optim import lr_scheduler
 
 from losses import encoder_loss, generator_loss, discriminator_loss
 from tqdm import tqdm
+import wandb
+from src.config import CFG as cfg
 
 
 def train_one_epoch(
-    model: nn.Module, 
-    optimizer: optim, 
-    scheduler: lr_scheduler, 
+    enc: nn.Module, gen: nn.Module, disc: nn.Module,
+    enc_opt: optim, gen_opt: optim, disc_opt: optim,
+    enc_scheduler: lr_scheduler, gen_scheduler: lr_scheduler, disc_scheduler: lr_scheduler, 
     trainloader: torch.utils.data.DataLoader, 
 ) -> float:
     
-    model.train()
+    enc.train()
+    gen.train()
+    disc.train()
 
+    pbar = tqdm(enumerate(trainloader), total=len(trainloader), desc=f'Train ')
+    for step, batch in pbar:
+        X = batch['x'].to(cfg.device), batch['y'].to(cfg.device)
+        latent, mu, std = enc.forward(X)
+        reconstruction = gen.forward(latent)
+
+        # encoder loss
+        encoder_loss = encoder_loss(mu=mu, std=std, gen_out=reconstruction, X=X)
 
     return 0
--- src/losses.py
+++ src/losses.py
@@ -1,15 +1,20 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-def encoder_loss():
+def encoder_loss(mu: torch.tensor, std: torch.tensor, gen_out: torch.tensor, X: torch.tensor):
+    kl_divergence = 0.5 * torch.sum(mu ** 2 + std ** 2 - torch.log(1e-8 + std ** 2) - 1) / torch.prod(X.shape)
+    gen_mse       = nn.MSELoss()(gen_out, X)
+    return kl_divergence + gen_mse
+
+
+def generator_loss(reconstruction: torch.tensor, X: torch.tensor, y: torch.tensor):
+    gen_loss = F.binary_cross_entropy(input=reconstruction, target=torch.ones_like(reconstruction))
+    gen_diff_loss = F.mse_loss(input=reconstruction, target=y)
+    return gen_loss + gen_diff_loss
+
+
+def discriminator_loss(reconstruction: torch.tensor, X: torch.tensor, y: torch.tensor):
+    # disc_real = F.binary_cross_entropy(input=y, target=)
     pass
-
-
-def generator_loss():
-    pass
-
-
-def discriminator_loss():
-    pass
-
